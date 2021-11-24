35232cc95c9ceb737f129c2beb3d980523c16cb6
briancervantes <mr.cervantesgaona@gmail.com>
2021-11-24 11:11:01 -0800

    added hover functionality to shop.html imgs

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -12,3 +12,39 @@ $(function() {
         }
     });
 });
+
+$(function() {
+    $("#change-img").hover(function(){
+        $( "#change-img" ).attr("src","/img/tshirt2.jpg");
+    }, function(){
+        $( "#change-img" ).attr("src","/img/tshirt1.jpg");
+    });
+});
+$(function() {
+    $("#change-img2").hover(function(){
+        $( "#change-img2" ).attr("src","/img/tshirt2.jpg");
+    }, function(){
+        $( "#change-img2" ).attr("src","/img/tshirt1.jpg");
+    });
+});
+$(function() {
+    $("#change-img3").hover(function(){
+        $( "#change-img3" ).attr("src","/img/Jacket2.jpg");
+    }, function(){
+        $( "#change-img3" ).attr("src","/img/Jacket1.jpg");
+    });
+});
+$(function() {
+    $("#change-img4").hover(function(){
+        $( "#change-img4" ).attr("src","/img/Jacket2.jpg");
+    }, function(){
+        $( "#change-img4" ).attr("src","/img/Jacket1.jpg");
+    });
+});
+$(function() {
+    $("#change-img5").hover(function(){
+        $( "#change-img5" ).attr("src","/img/Jacket2.jpg");
+    }, function(){
+        $( "#change-img5" ).attr("src","/img/Jacket1.jpg");
+    });
+});
